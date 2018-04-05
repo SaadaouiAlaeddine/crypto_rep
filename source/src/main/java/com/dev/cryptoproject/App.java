91bd209a67ee81d9a1f6541fd20683e49444a1d5
alaeddine <3alaeddine@gmail.com>
2018-04-05 13:45:21 -0500

    Commit source code and applications components

---
+++ source/src/main/java/com/dev/cryptoproject/App.java
@@ -0,0 +1,52 @@
+package com.dev.cryptoproject;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * @Author: Alaeddine Saadaoui
+ * @ProjectName: Cryptocurrency project
+ *
+ */
+public class App
+{
+    private static final String apiURL="https://api.coinmarketcap.com/v1/ticker/";
+    public static void processCryptoCurrencyName(String cryptoName)
+    {
+        String cryptoServiceEndpoint=apiURL+cryptoName+"/";
+        Client client=ClientBuilder.newClient();;
+        WebTarget target=client.target(cryptoServiceEndpoint);;
+        Response response=target.request(MediaType.APPLICATION_JSON).get();
+        if(response.getStatus()!=Response.Status.OK.getStatusCode())
+        {
+            System.out.println("the provided crypto-currency does not exist");
+        }
+        else
+        {
+            List<CryptoCurrency> cryptList=response.readEntity(new GenericType<ArrayList<CryptoCurrency>>(){});
+            System.out.println("The provided crypto-currency: "+cryptoName);
+            System.out.println("Price(USD): $"+cryptList.get(0).getPrice_usd());
+            System.out.println("Market Cap (USD): $"+cryptList.get(0).getMarket_cap_usd());
+        }
+
+        response.close();
+        client.close();
+
+    }
+    public static void main( String[] args )
+    {
+        if(args.length!=1||(args.length==1&&args[0].equals("default")))
+          System.out.println( "Usage: docker run -rm cryptoapp [crypto-currency name]" );
+        else
+            processCryptoCurrencyName(args[0]);
+
+    }
+}
---
+++ source/src/main/java/com/dev/cryptoproject/CryptoCurrency.java
@@ -0,0 +1,45 @@
+package com.dev.cryptoproject;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.io.Serializable;
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CryptoCurrency implements Serializable {
+
+
+    @JsonProperty("price_usd")
+    String price_usd;
+    @JsonProperty("market_cap_usd")
+    String market_cap_usd;
+
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("CryptoCurrency{");
+        sb.append(", price_usd=").append(price_usd);
+        sb.append(", market_cap_usd=").append(market_cap_usd);
+        sb.append('}');
+        return sb.toString();
+    }
+
+
+    public void setPrice_usd(String price_usd) {
+        this.price_usd = price_usd;
+    }
+    public String getPrice_usd() {
+        return price_usd;
+    }
+
+    public String getMarket_cap_usd() {
+        return market_cap_usd;
+    }
+
+    public void setMarket_cap_usd(String market_cap_usd) {
+        this.market_cap_usd = market_cap_usd;
+    }
+
+
+
+
+}
